3908a05a5f5a0e020957a4d9a571c5b59d94fc77
harmless <harmless@syneforge.com>
2017-01-27 12:09:14 +0200

    Fix style textarea

--- register.php
+++ register.php
@@ -107,9 +107,7 @@ if ($settings['enable_registration'] == 0) {
     <tr>
      <td colspan='2'>
       <center>
-       <textarea style='height:40px;width:585px;' onclick='this.select()' readonly=''>
-        <a href='http://contra.net.ua/' target='_blank'&gt;Игровые сервера cs 1.6 Украина</a>
-       </textarea>
+       <textarea style='height:40px;width:585px;' onclick='this.select()' readonly=''><a href='http://contra.net.ua/' target='_blank'&gt;Игровые сервера cs 1.6 Украина</a></textarea>
       </center>
      </td>
     </tr>
