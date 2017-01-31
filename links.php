79072a1bad747edecdd46eed7c012780ca2caeeb
harmless <harmless@syneforge.com>
2017-01-31 12:59:39 +0200

    Cleanup

--- links.php
+++ links.php
@@ -5,59 +5,59 @@
 */
 
 /* Script security */
-if(!defined("MONENGINE")) {
-	header("Location: index.php");
-	exit();
+if (!defined("MONENGINE")) {
+ header("Location: index.php");
+ exit();
 }
 ?>
 <div class='horizontal_line'>Каталог ссылок</div>
 <div class='cont'>
-        
-
-
-
-<table style="font-family: Verdana, Tahoma; font-size: 12px; padding: 0px; border: 0px;" cellspacing="0" cellpadding="0" width="100%">
-<p>
-<div style="padding-top:20px;">&nbsp;</div>
-	<div style="overflow:hidden;width:800px;" align="justify"><div style="padding:10px;">
-		<div style="padding-bottom:10px;">По вопросам размещения вашей кнопки или ссылки в каталоге - пишите в <b>skype: vengeanson</b>. В каталог заносятся только качественные сайты. Качественный ресурс не должен содержать ворованного материала, иметь собственный уникальный контент, занимать достойное местно в рейтингах.</div>
-		<div style="padding-bottom:10px;"><b>При возникновении битой ссылки либо битого адреса кнопки, сайт исключается из каталога.</b></div>
-		<div>Код нашей кнопки:</div>
-		<div style="padding-bottom:10px;"><textarea cols="35" rows="6"><a href="http://contra.net.ua"><img border="0" src="http://www.contra.net.ua/Foto/1.gif" width="130" height="30" alt="Игровой сайт для всех"></a></textarea></div>
-		<div>Пример кнопки:</div>
-		<div style="padding-top:5px;"><a href="http://contra.net.ua"><img border="0"src="http://www.contra.net.ua/Foto/1.gif" width="130" height="30" alt="Игровой сайт для всех"></a>
-</div>
-<br>
-<div>
-<b>1.</b> Инвестиции в <a href="http://prvigroup.ua/vino.html">виноградники во Франции</a>.
-</div>
-<div>
-<b>2.</b> <a href="http://israelmedicin.ru/">Израиль: лучшая медицина по доступной цене.</a>
-</div>
-<div>
-<b>3.</b> Ремонт дисков <a href="http://www.raznyesamodelki.ru/">своими руками</a>
-</div>
-<div>
-<b>4.</b> Читайте также : <a href="http://studio-didier.com/" title="studio-didier.com">studio-didier.com</a><br>
-<b>5.</b> Источник: <a href="http://ramamama.ru/kak-pravilno-nosit-stringi/">как правильно носить стринги</a>.
-</div>
-	</div></div>
-
-<div>
-
-</div>
-
-
+ <p>
+  <div style="padding-top:20px;">&nbsp;</div>
+  <div style="overflow:hidden;width:800px;" align="justify">
+   <div style="padding:10px;">
+    <div style="padding-bottom:10px;">По вопросам размещения вашей кнопки или ссылки в каталоге - пишите в <b>skype: vengeanson</b>. В каталог заносятся только качественные сайты. Качественный ресурс не должен содержать ворованного материала, иметь собственный уникальный контент, занимать достойное местно в рейтингах.</div>
+    <div style="padding-bottom:10px;">
+     <b>При возникновении битой ссылки либо битого адреса кнопки, сайт исключается из каталога.</b>
+    </div>
+    <div>Код нашей кнопки:</div>
+    <div style="padding-bottom:10px;">
+     <textarea cols="35" rows="6">
+     <a href="http://contra.net.ua">
+      <img border="0" src="http://www.contra.net.ua/Foto/1.gif" width="130" height="30" alt="Игровой сайт для всех">
+     </a>
+     </textarea>
+    </div>
+    <div>Пример кнопки:</div>
+    <div style="padding-top:5px;">
+     <a href="http://contra.net.ua">
+      <img border="0"src="http://www.contra.net.ua/Foto/1.gif" width="130" height="30" alt="Игровой сайт для всех">
+     </a>
+    </div>
+    <br>
+    <div>
+     <b>1.</b> Инвестиции в <a href="http://prvigroup.ua/vino.html">виноградники во Франции</a>.
+    </div>
+    <div>
+     <b>2.</b> <a href="http://israelmedicin.ru/">Израиль: лучшая медицина по доступной цене.</a>
+    </div>
+    <div>
+     <b>3.</b> Ремонт дисков <a href="http://www.raznyesamodelki.ru/">своими руками</a>
+    </div>
+    <div>
+     <b>4.</b> Читайте также : <a href="http://studio-didier.com/" title="studio-didier.com">studio-didier.com</a><br>
+     <b>5.</b> Источник: <a href="http://ramamama.ru/kak-pravilno-nosit-stringi/">как правильно носить стринги</a>.
+    </div>
+   </div>
+  </div>
+  <div>
+ </div>
 </p>
-</tbody>
-</table>﻿
 <center>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- CS 1.6 как лучшая игра -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:320px;height:100px"
-     data-ad-client="ca-pub-0760255674877926"
-     data-ad-slot="4806637623"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script></center>
+ <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+ <!-- CS 1.6 как лучшая игра -->
+ <ins class="adsbygoogle" style="display:inline-block;width:320px;height:100px" data-ad-client="ca-pub-0760255674877926" data-ad-slot="4806637623"></ins>
+ <script>
+  (adsbygoogle = window.adsbygoogle || []).push({});
+ </script>
+</center>
