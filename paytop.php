81b8b12b6140293e86cad3d1a2a540b1320580fa
harmless <harmless@syneforge.com>
2017-01-31 18:23:10 +0200

    Cleanup

--- paytop.php
+++ paytop.php
@@ -9,7 +9,8 @@ if (!defined("MONENGINE")) {
  header("Location: index.php");
  exit();
 }
-?>
+
+echo <<<EOT
 <div class='horizontal_line'>Заказать «Премиум место»</div>
 <div class='cont'>
  <div class="msg greenbg">Приобрести данное место Вы можете <b>бесплатно</b>. Для заказа места обратитесь в Skype: <b>vengeanson</b></div>
@@ -21,3 +22,4 @@ if (!defined("MONENGINE")) {
    </tr>
   </table>
 </div>
+EOT;
