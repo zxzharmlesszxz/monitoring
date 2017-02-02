81e6de7340306683c7f18abb78899b12f46e1088
harmless <harmless@syneforge.com>
2017-02-02 12:14:29 +0200

    Cleanup

--- banned.php
+++ banned.php
@@ -5,20 +5,21 @@
 */
 
 $take_server = dbquery("SELECT * FROM ".DB_SERVERS." WHERE server_id = ".$_GET['id']."");
-			$server_data = dbarray_fetch($take_server);
+$server_data = dbarray_fetch($take_server);
 
 /* Other code */
-echo "<div class='horizontal_line'>Cервер заблокирован</div>";
-	echo "<div class='cont'>";
-echo "<div class='msg redbg'>Сервер <b>$server_data[server_name]</b> заблокирован. Для выяснения причины Skype: monservers или Icq: 162-622</div>";
-echo "		<table width='100%' height='150'>";
-echo "			<tr><td valign='middle'>";
-echo "				<td align='center' style='font-size:16px;'><a href='/'>Вернуться на главную страницу</a></td>";
-echo "			</td></tr>";
-echo "		</table>";
-echo "	</div>";
-
-
-
-
-?>
+echo <<<EOT
+<div class='horizontal_line'>Cервер заблокирован</div>
+<div class='cont'>
+ <div class='msg redbg'>Сервер <b>{$server_data['server_name']}</b> заблокирован. Для выяснения причины Skype: monservers или Icq: 162-622</div>
+ <table width='100%' height='150'>
+  <tr>
+   <td valign='middle'>
+   </td>
+   <td align='center' style='font-size:16px;'>
+    <a href='/'>Вернуться на главную страницу</a>
+   </td>
+  </tr>
+ </table>
+</div>
+EOT;
