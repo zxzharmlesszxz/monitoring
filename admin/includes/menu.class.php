7bb09ef17702b7f228d210999416363cf7b5fdb5
harmless <harmless@syneforge.com>
2017-02-02 13:54:52 +0200

    Cleanup

--- admin/includes/menu.class.php
+++ admin/includes/menu.class.php
@@ -6,31 +6,31 @@
 
 /* Script security */
 if (!defined("MONENGINE")) {
-	header("Location: index.php");
-	exit();
+ header("Location: index.php");
+ exit();
 }
 
 /* CLASS CODE */
 
 class Menu {
-	public $pages = Array();
-	public $curpage;
-	
-	public function show() {
-		foreach ($this->pages as $page) {
+ public $pages = Array();
+ public $curpage;
+ 
+ public function show() {
+  foreach ($this->pages as $page) {
     $class = ($page['name'] == $this->curpage) ? "class='current'" : "";
-			echo "<li {$class}><a href='{$page['url']}' title='{$page['descr']}'>{$page['title']}</a></li>";
-		}
-	}
-	
-	public function set($name) {
-		$this->curpage = $name;
-	}
-	
-	public function add($name, $url, $title, $descr='') {
-		$this->pages[$name]['url'] = $url;
-		$this->pages[$name]['title'] = $title;
-		$this->pages[$name]['descr'] = $descr;
-		$this->pages[$name]['name'] = $name;
+   echo "<li {$class}><a href='{$page['url']}' title='{$page['descr']}'>{$page['title']}</a></li>";
+  }
+ }
+ 
+ public function set($name) {
+  $this->curpage = $name;
+ }
+ 
+ public function add($name, $url, $title, $descr='') {
+  $this->pages[$name]['url'] = $url;
+  $this->pages[$name]['title'] = $title;
+  $this->pages[$name]['descr'] = $descr;
+  $this->pages[$name]['name'] = $name;
  }
 }
