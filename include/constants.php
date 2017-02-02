fa7b169d97bd19fc5f7bdc0cf417d594a6548113
harmless <harmless@syneforge.com>
2017-02-02 13:10:14 +0200

    Cleanup

--- include/constants.php
+++ include/constants.php
@@ -2,7 +2,18 @@
 // Константы
 define("BASEDIR", __DIR__.'/../');
 define("ENGINE_SELF", basename($_SERVER['PHP_SELF']));
+// Константы таблиц БД
 define("QUOTES_GPC", (ini_get('magic_quotes_gpc') ? TRUE : FALSE));
+define("DB_ADMIN", DB_PREFIX."admin");
+define("DB_BLACKLIST", DB_PREFIX."blacklist");
+define("DB_MESSAGES", DB_PREFIX."messages");
+define("DB_SETTINGS", DB_PREFIX."settings");
+define("DB_USERS", DB_PREFIX."users");
+define("DB_SERVERS", DB_PREFIX."servers");
+define("DB_VOTES", DB_PREFIX."vote_ip");
+define("DB_COMMENTS", DB_PREFIX."comments");
+define("DB_ROWSTYLES", DB_PREFIX."rowstyles");
+define("DB_SERVERS_EDITS", DB_PREFIX."servers_edits");
 $settings = dbarray(dbquery("SELECT * FROM ".DB_SETTINGS));
 define("SITE_URL", $settings['site_url']);
 
@@ -18,14 +29,4 @@ define("LOCALESET", $settings['locale']."/");
 define("USER_IP", $_SERVER['REMOTE_ADDR']);
 define("ASR",$settings['AMX']);
 
-// Константы таблиц БД
-define("DB_ADMIN", DB_PREFIX."admin");
-define("DB_BLACKLIST", DB_PREFIX."blacklist");
-define("DB_MESSAGES", DB_PREFIX."messages");
-define("DB_SETTINGS", DB_PREFIX."settings");
-define("DB_USERS", DB_PREFIX."users");
-define("DB_SERVERS", DB_PREFIX."servers");
-define("DB_VOTES", DB_PREFIX."vote_ip");
-define("DB_COMMENTS", DB_PREFIX."comments");
-define("DB_ROWSTYLES", DB_PREFIX."rowstyles");
-define("DB_SERVERS_EDITS", DB_PREFIX."servers_edits");
+
