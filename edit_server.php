afd4da088c7c41d0d209acff27f4fc31989ff147
harmless <harmless@syneforge.com>
2017-02-02 18:50:18 +0200

    Cleanup

--- edit_server.php
+++ edit_server.php
@@ -5,176 +5,188 @@
 */
 
 /* Script security */
-if(!defined("MONENGINE")) {
-	header("Location: index.php");
-	exit();
+if (!defined("MONENGINE")) {
+ header("Location: index.php");
+ exit();
 }
 /* Other code */
 
 $stage = 1;
-if(isset($_GET['page']) and $_GET['page'] == 'edit_server' and isset($_GET['secret_key'])) $stage = 2;
+if (isset($_GET['page']) and $_GET['page'] == 'edit_server' and isset($_GET['secret_key'])) $stage = 2;
 
-if($stage == 1) {
-	echo "
-	<div class='horizontal_line'>Запрос на редактирование сервера<br><br><br></div>
-	<div class='cont'>
-	";
-	if(isset($_POST['submit_edit_form'])) {
-		require_once(INCLUDES."server_edit.class.php");
-		$edit = new ServerEdit;
-		$edit->SetServerId($_POST['id']);
-		$edit->SetServerEmail($_POST['email']);
-		if(!$edit->CheckData()) {
-			echo "<div class='msg redbg'>{$edit->errors[0]}</div>";
-		} else {
-			if(count($edit->errors) != 0) {
-				echo "<div class='msg redbg'>{$edit->errors[0]}</div>";
-			} else {
+if ($stage == 1) {
+ echo "
+ <div class='horizontal_line'>Запрос на редактирование сервера<br><br><br></div>
+ <div class='cont'>";
 
-				if($edit->SetupKey()) {
-					if(!isset($_SESSION['captcha_keystring']) or $_SESSION['captcha_keystring'] != $_POST['keystring']){
-						echo "<div class='msg redbg'>Вы неверно ввели текст с картинки.<br><br><br></div>";
-					}
-					else
-					{
-						if($edit->SendMail()) echo "<div class='msg greenbg'>Сообщение с инструкциями было отправлено Вам на почту.<br><br><br></div>";
-					}
-				} else {
-					
-				}
-			}
-		}
-	}
+ if (isset($_POST['submit_edit_form'])) {
+  require_once(INCLUDES."server_edit.class.php");
+  $edit = new ServerEdit;
+  $edit->SetServerId($_POST['id']);
+  $edit->SetServerEmail($_POST['email']);
+  if (!$edit->CheckData()) {
+   echo "<div class='msg redbg'>{$edit->errors[0]}</div>";
+  } else {
+   if (count($edit->errors) != 0) {
+    echo "<div class='msg redbg'>{$edit->errors[0]}</div>";
+   } else {
 
-	echo "
-		<form action='' method='POST'>
-		<table align='center' class='editform'>
-			<tr>
-				<td colspan='2'></td>
-			</tr>
-			<tr>
-				<td align='right'><br><br><br>ID вашего сервера:<br><br><br></td>
-				<td><input type='text' name='id' value='' size='30' /></td>
-			</tr>
-			<tr>
-				<td align='right'><br><br><br>Ваш E-mail адрес:<br><br><br></td>
-				<td><input type='text' name='email' value='' size='30' /></td>
-			</tr>
-				<tr>
-				<td align='right'><br><br><br>Код с картинки:<br><br><br></td>
-				<td><img src='cap/index.php?".session_name()."=".session_id()."'></td>
-			</tr>
-			<tr>
-				<td align='right'>&nbsp;</td>
-				<td><input type='text' style='width:160px;' name='keystring' /></td>
-			</tr>
-			<tr>
-				<input type='hidden' name='submit_edit_form' value='1'>
-				<td colspan='2' align='center'><input type='submit' value='Отправить запрос' /></td>
-			</tr>
-		</table>
-		</form>
-	</div>";
-} elseif($stage == 2) {
-	echo "	<div class='horizontal_line'><br><br><br>Редактирование сервера<br><br><br></div>
-			<div class='cont'>
-	";
-	$secret_key = $_GET['secret_key'];
-	require_once(INCLUDES."server_edit.class.php");
-	$edit = new ServerEdit;
-	
-	if($edit->CheckKey($secret_key)) {
-		$finish = false;
-		if(isset($_POST['submit_edit_form'])) {
-			if($edit->SaveChanges($_POST['address'], $_POST['icq'], $_POST['site'], $_POST['about'], $_POST['server_game'], $_POST['server_mode'])) {
-				echo "<div class='msg greenbg'>Изменения успешно сохранены.</div><br><center><a href='server/{$edit->server_data['server_id']}'>Перейти на страницу сервера</a></center><br>";
-				$finish = true;
-			} elseif(count($edit->save_errors) > 0) {
-				echo "<div class='msg redbg'>{$edit->save_errors[0]}</div>";
-			} else {
-				echo "<div class='msg redbg'>Произошла неизвестная ошибка. Обратитесь к Администрации.</div>";
-			}
-		}
-		if(!$finish) {
-			echo "
-				<form action='' method='POST'>
-				<table align='center' class='editform'>
-				
-		<tr>
-<td align='right'>Игра сервера:</td>
-				<td>
-<select style='width:200px;' name='server_game' value='{$edit->server_data['server_game']}'>
-     <option value='cs16'>Counter-Strike</option> 
-     <option value='css'>Counter-Strike: Source</option> 
-	 <option value='cz'>Counter Strike: Condition zero</option>
-	 <option value='csgo'>Counter Strike: Global Offensive</option>
-     <option value='hl2dm'>Half-Life 2</option> 
-     <option value='hl2dm'>Half-Life 2 DM</option> 
-     <option value='l4d'>Left 4 Dead</option> 
+    if ($edit->SetupKey()) {
+     if (!isset($_SESSION['captcha_keystring']) or $_SESSION['captcha_keystring'] != $_POST['keystring']) {
+      echo "<div class='msg redbg'>Вы неверно ввели текст с картинки.<br><br><br></div>";
+     } else {
+      if ($edit->SendMail()) echo "<div class='msg greenbg'>Сообщение с инструкциями было отправлено Вам на почту.<br><br><br></div>";
+     }
+    } else {
+     
+    }
+   }
+  }
+ }
+
+ echo "
+  <form action='' method='POST'>
+  <table align='center' class='editform'>
+   <tr>
+    <td colspan='2'></td>
+   </tr>
+   <tr>
+    <td align='right'><br><br><br>ID вашего сервера:<br><br><br></td>
+    <td><input type='text' name='id' value='' size='30' /></td>
+   </tr>
+   <tr>
+    <td align='right'><br><br><br>Ваш E-mail адрес:<br><br><br></td>
+    <td><input type='text' name='email' value='' size='30' /></td>
+   </tr>
+    <tr>
+    <td align='right'><br><br><br>Код с картинки:<br><br><br></td>
+    <td><img src='cap/index.php?".session_name()."=".session_id()."'></td>
+   </tr>
+   <tr>
+    <td align='right'>&nbsp;</td>
+    <td><input type='text' style='width:160px;' name='keystring' /></td>
+   </tr>
+   <tr>
+    <input type='hidden' name='submit_edit_form' value='1'>
+    <td colspan='2' align='center'><input type='submit' value='Отправить запрос' /></td>
+   </tr>
+  </table>
+  </form>
+ </div>";
+} elseif ($stage == 2) {
+ echo " <div class='horizontal_line'>Редактирование сервера</div>
+   <div class='cont'>";
+
+ $secret_key = $_GET['secret_key'];
+ require_once(INCLUDES."server_edit.class.php");
+ $edit = new ServerEdit;
+ 
+ if ($edit->CheckKey($secret_key)) {
+  $finish = false;
+  if (isset($_POST['submit_edit_form'])) {
+   if ($edit->SaveChanges($_POST['address'], $_POST['icq'], $_POST['site'], $_POST['about'], $_POST['server_game'], $_POST['server_mode'])) {
+    echo "<div class='msg greenbg'>Изменения успешно сохранены.</div><br><center><a href='server/{$edit->server_data['server_id']}'>Перейти на страницу сервера</a></center><br>";
+    $finish = true;
+   } elseif (count($edit->save_errors) > 0) {
+    echo "<div class='msg redbg'>{$edit->save_errors[0]}</div>";
+   } else {
+    echo "<div class='msg redbg'>Произошла неизвестная ошибка. Обратитесь к Администрации.</div>";
+   }
+  }
+
+  if (!$finish) {
+   echo "
+<form action='' method='POST'>
+ <table align='center' class='editform'>
+  <tr>
+   <td align='right'>Игра сервера:</td>
+   <td>
+    <select style='width:200px;' name='server_game' value='{$edit->server_data['server_game']}'>
+     <option value='cs16'>Counter-Strike</option>
+     <option value='css'>Counter-Strike: Source</option>
+     <option value='cz'>Counter Strike: Condition zero</option>
+     <option value='csgo'>Counter Strike: Global Offensive</option>
+     <option value='hl2dm'>Half-Life 2</option>
+     <option value='hl2dm'>Half-Life 2 DM</option>
+     <option value='l4d'>Left 4 Dead</option>
      <option value='l4d2'>Left 4 Dead 2</option>
-     <option value='tf2'>Team Fortress 2</option> 
+     <option value='tf2'>Team Fortress 2</option>
      <option value='gm'>Garry's Mod</option>
-     </select></td>	</tr>
-	 
-	 <tr>
-<td align='right'>Мод сервера:</td>
-				<td>
-<select style='width:200px;' name='server_mode' value='{$edit->server_data['server_mode']}'>
-<option value='classic'>Classic</option>
-<option value='warcraft'>Warcraft</option>
-<option value='csdm'>CSDM</option>
-<option value='gungame'>GunGame</option>
-<option value='hns'>HNS</option>
-<option value='surf'>Surf</option>
-<option value='jump'>Jump</option>
-<option value='deathrun'>Deathrun</option>
-<option value='diablomod'>DiabloMod</option>
-<option value='superhero'>SuperHero</option>
-<option value='soccerjam'>Soccer Jam</option>
-<option value='jailbreak'>JailBreak</option>
-<option value='knife'>Knife</option>
-<option value='zombiemod'>Zombie Mod</option>
-
-     </select></td>	</tr>
-				
-					<tr>
-						<td align='right'>IP:PORT сервера</td>
-						<td><input type='text' name='address' value='{$edit->server_data['server_ip']}' size='40'></td>
-					</tr>
-					<tr>
-						<td align='right'>ICQ:</td>
-						<td><input type='text' name='icq' value='{$edit->server_data['server_icq']}' size='20'></td>
-					</tr>
-					<tr>
-						<td align='right'>Сайт сервера:</td>
-						<td><input type='text' name='site' value='{$edit->server_data['server_site']}' size='40'></td>
-					</tr>
-					
-					
-					<tr>
-						<td align='right'>Информация о сервере:</td>
-						<td><textarea name='about' cols='50'>{$edit->server_data['about']}</textarea></td>
-					</tr>
-									<tr>
-				<td align='right'>Введите код с картинки:</td>
-				<td><img src='cap/index.php?".session_name()."=".session_id()."'></td>
-			</tr>
-			<tr>
-				<td align='right'>&nbsp;</td>
-				<td><input type='text' style='width:160px;' name='keystring' /></td>
-			</tr>
-					<tr>
-						<input type='hidden' name='submit_edit_form' value='1'>
-						<td colspan='2' align='center'><input type='submit' value='Сохранить изменения' /></td>
-					</tr>
-				</table>
-				</form>
-			";
-		}
-	} else {
-		echo "<div class='msg redbg'>Данный секретный ключ не существует.</div><br><center><a href='index.php'>Вернуться на главную страницу</a></center><br>";
-	}
-	
+    </select>
+   </td>
+  </tr>
+  <tr>
+   <td align='right'>Мод сервера:</td>
+   <td>
+    <select style='width:200px;' name='server_mode' value='{$edit->server_data['server_mode']}'>
+     <option value='classic'>Classic</option>
+     <option value='warcraft'>Warcraft</option>
+     <option value='csdm'>CSDM</option>
+     <option value='gungame'>GunGame</option>
+     <option value='hns'>HNS</option>
+     <option value='surf'>Surf</option>
+     <option value='jump'>Jump</option>
+     <option value='deathrun'>Deathrun</option>
+     <option value='diablomod'>DiabloMod</option>
+     <option value='superhero'>SuperHero</option>
+     <option value='soccerjam'>Soccer Jam</option>
+     <option value='jailbreak'>JailBreak</option>
+     <option value='knife'>Knife</option>
+     <option value='zombiemod'>Zombie Mod</option>
+    </select>
+   </td>
+  </tr>
+  <tr>
+   <td align='right'>IP:PORT сервера</td>
+   <td>
+    <input type='text' name='address' value='{$edit->server_data['server_ip']}' size='40'>
+   </td>
+  </tr>
+  <tr>
+   <td align='right'>ICQ:</td>
+   <td>
+    <input type='text' name='icq' value='{$edit->server_data['server_icq']}' size='20'>
+   </td>
+  </tr>
+  <tr>
+   <td align='right'>Сайт сервера:</td>
+   <td>
+    <input type='text' name='site' value='{$edit->server_data['server_site']}' size='40'>
+   </td>
+  </tr>
+  <tr>
+   <td align='right'>Информация о сервере:</td>
+   <td>
+    <textarea name='about' cols='50'>{$edit->server_data['about']}</textarea>
+   </td>
+  </tr>
+  <tr>
+   <td align='right'>Введите код с картинки:</td>
+   <td>
+    <img src='cap/index.php?".session_name()."=".session_id()."'>
+   </td>
+  </tr>
+  <tr>
+   <td align='right'>&nbsp;</td>
+   <td>
+    <input type='text' style='width:160px;' name='keystring' />
+   </td>
+  </tr>
+  <tr>
+   <td colspan='2' align='center'>
+    <input type='hidden' name='submit_edit_form' value='1'>
+    <input type='submit' value='Сохранить изменения' />
+   </td>
+  </tr>
+ </table>
+</form>";
+  }
+ } else {
+  echo "<div class='msg redbg'>Данный секретный ключ не существует.</div>
+  <br>
+  <center>
+   <a href='index.php'>Вернуться на главную страницу</a>
+  </center>
+  <br>";
+ }
 }
-
-?>
--- listing.php
+++ listing.php
@@ -1,46 +1,62 @@
-	<?php
-				echo "
-					<div style='padding:10px;border-top:1px solid #4F4F4F;'>
-			<style>.topserv_list li, .viplist li {padding-bottom:10px;}</style>
-			<style>.topserv_list span {color:#3ECBFA;}</style>
-			<style>.viplist span {color:#AAAAAA;}</style>
-			<div style='color:#1584C4;'>
-				<div style='font-size:20px;'>
-<div class='listingtop'><b>Листинг серверов в шапке сайта (текущее время на сервере ".date("d.m.Y H:i:s")."):</b>
-<ol>";
-for($itop=1;$itop<5 * LINES_NUM+1;$itop++)
-{
-		$rtop = mysql_fetch_array(mysql_query("SELECT * FROM `mon_servers` WHERE server_top=$itop"));
-		if($rtop['server_top']) 
-		{
-			echo "<li>Место занято сервером <a href='/server/{$rtop['server_id']}/'>{$rtop['server_name']}</a>. Освободится через ".time2string($rtop['server_top_time'] - time(),true,false,false,false)." (".formatDate("d.m.Y H:i:s",$rtop['server_top_time']).")</li>";
-		}
-		else
-		{
-			echo "<li>Место свободно!</li>";
-		}
+<?php
+
+echo "
+ <div style='padding:10px;border-top:1px solid #4F4F4F;'>
+  <style>
+   .topserv_list li, .viplist li {padding-bottom:10px;}
+   .topserv_list span {color:#3ECBFA;}
+   .viplist span {color:#AAAAAA;}
+  </style>
+  <div style='color:#1584C4;'>
+  <div style='font-size:20px;'>
+   <div class='listingtop'>
+    <b>Листинг серверов в шапке сайта (текущее время на сервере ".date("d.m.Y H:i:s")."):</b>
+    <ol>";
+
+for ($itop=1;$itop<5 * LINES_NUM+1;$itop++) {
+ $rtop = mysql_fetch_array(mysql_query("SELECT * FROM `mon_servers` WHERE server_top=$itop"));
+
+ if ($rtop['server_top']) {
+  echo "<li>Место занято сервером <a href='/server/{$rtop['server_id']}/'>{$rtop['server_name']}</a>. Освободится через ".time2string($rtop['server_top_time'] - time(),true,false,false,false)." (".formatDate("d.m.Y H:i:s",$rtop['server_top_time']).")</li>";
+ } else {
+  echo "<li>Место свободно!</li>";
+ }
 }
+
 echo "</ol>
 </div>
-<div class='listingvip'><h2><b>Листинг VIP серверов:</b></h2>
-<ol>";
-$qvip = mysql_query("SELECT * FROM `mon_servers` WHERE server_vip=1");
-if(mysql_num_rows($qvip) == 1) echo "<li>Все места свободны</li>";
-while($rvip = mysql_fetch_array($qvip))
-{
-echo "<li>Место занято сервером <a href='/server/{$rvip['server_id']}/'>{$rvip['server_name']}</a>. Освободится через ".time2string($rvip['server_vip_time'] - time(),true,false,false,false)." (".formatDate("d.m.Y H:i:s",$rvip['server_vip_time']).")</li>";	
-}
-echo "</ol>
-</div>	
-<div class='listingcolor'><h2><b>Выделенные серверы:</b></h2>
-<ol>";
-$qcolor = mysql_query("SELECT * FROM `mon_servers` WHERE server_row_style RLIKE '_'");
-if(mysql_num_rows($qcolor) == 1) echo "<li>Все места свободны</li>";
-while($rcolor = mysql_fetch_array($qcolor))
-{
-echo "<li>Место занято сервером <a href='/server/{$rcolor['server_id']}/'>{$rcolor['server_name']}</a>. Освободится через ".time2string($rcolor['server_color_time'] - time(),true,false,false,false)." (".formatDate("d.m.Y H:i:s",$rcolor['server_color_time']).")</li>";	
-}
-echo "</ol>
-";
+<div class='listingvip'>
+ <h2>
+  <b>Листинг VIP серверов:</b>
+ </h2>
+ <ol>";
 
-?>
+$qvip = mysql_query("SELECT * FROM `mon_servers` WHERE server_vip=1");
+
+if (mysql_num_rows($qvip) == 1) {
+ echo "<li>Все места свободны</li>";
+}
+
+while ($rvip = mysql_fetch_array($qvip)) {
+ echo "<li>Место занято сервером <a href='/server/{$rvip['server_id']}/'>{$rvip['server_name']}</a>. Освободится через ".time2string($rvip['server_vip_time'] - time(),true,false,false,false)." (".formatDate("d.m.Y H:i:s",$rvip['server_vip_time']).")</li>";
+}
+
+echo "</ol>
+</div> 
+<div class='listingcolor'>
+ <h2>
+  <b>Выделенные серверы:</b>
+ </h2>
+ <ol>";
+
+$qcolor = mysql_query("SELECT * FROM `mon_servers` WHERE server_row_style RLIKE '_'");
+
+if (mysql_num_rows($qcolor) == 1) {
+ echo "<li>Все места свободны</li>";
+}
+
+while ($rcolor = mysql_fetch_array($qcolor)) {
+ echo "<li>Место занято сервером <a href='/server/{$rcolor['server_id']}/'>{$rcolor['server_name']}</a>. Освободится через ".time2string($rcolor['server_color_time'] - time(),true,false,false,false)." (".formatDate("d.m.Y H:i:s",$rcolor['server_color_time']).")</li>";
+}
+
+echo "</ol>";
