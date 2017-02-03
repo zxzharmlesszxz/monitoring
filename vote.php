91b31cf8879b20a367bf6d8b9a2356d66aa79825
harmless <harmless@syneforge.com>
2017-02-03 14:56:51 +0200

    Fix include core

--- vote.php
+++ vote.php
@@ -3,7 +3,7 @@
 if ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
  define("MONENGINE", true);
  
- require_once("/include/core.php");
+ require_once(__DIR__."/include/core.php");
  
  $hash = $_POST['hash'];
  $id = mysql_real_escape_string($_POST['id']);
