f5862c880a1f875f1912b20fcd83a50ea041b437
harmless <harmless@syneforge.com>
2017-02-07 19:15:35 +0200

    Add admin page what maps files images needed

---
+++ admin/needed_maps.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Server info display script
+ * Made by starky
+*/
+
+/* Script security */
+if (!defined("MONENGINE")) {
+ header("Location: index.php");
+ exit();
+}
+
+echo <<<EOT
+<b>Page was be here</b>
+EOT;
