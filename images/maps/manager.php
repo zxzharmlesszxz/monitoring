d29dc55c5e7534c1f5829fa558adff09ecf865b4
harmless <harmless@syneforge.com>
2017-02-07 14:13:44 +0200

    Cleanup(Oh my mind)

--- images/maps/manager.php
+++ images/maps/manager.php
@@ -7,7 +7,7 @@ if (isset($_GET['game']) and isset($_GET['map'])) {
   readfile($game."/".$map.".jpg");
  } elseif (file_exists($game."/".$map.".png")) {
   header("Content-Type: image/png");
-  readfile($game."/".$map.".jpg");
+  readfile($game."/".$map.".png");
  } else {
   header("Content-Type: image/gif");
   readfile("no_map.gif");
