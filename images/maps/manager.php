99eb3fd28a248bd79fa6bc3e4e0867386e752596
harmless <harmless@syneforge.com>
2017-02-08 13:06:59 +0200

    Fix view

--- images/maps/manager.php
+++ images/maps/manager.php
@@ -4,6 +4,6 @@ require_once(__DIR__.'/../../include/core.php');
 if (isset($_GET['game']) and isset($_GET['map'])) {
  $game = $_GET['game'];
  $map = $_GET['map'];
- check_map_image($map, $game);
+ var_dump(check_map_image($map, $game));
  get_map_image($map, $game);
 }
