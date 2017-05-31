18d6a7ad0ef018fbb26503cba66456c7ed1748f1
Harmless <harmless@syneforge.com>
2017-05-31 14:03:44 +0300

    Fix

--- cron.php
+++ cron.php
@@ -17,33 +17,27 @@ while ($r = db()->fetch_array($query)) {
 foreach ($servers as $num => $server) {
     $serv = serverInfo($server['server_ip']);
     if ($serv['status'] == 'off' || empty($serv['name'])) {
-        $result = db()->query("UPDATE " . DB_SERVERS . "
-   SET
-    server_status = '0',
-    server_map = '-',
-    server_players = '-',
-    server_maxplayers = '-'
-    " . (($server['server_status'] == 1 or $server['status_change'] == 0) ? ", status_change = " . time() : ", status_change = 10") . "
-   WHERE server_id='" . $server['server_id'] . "'");
+        $str = "UPDATE " . DB_SERVERS . " SET server_status = '0', server_map = '-', server_players = '-', server_maxplayers = '-' ";
+        $str .= (($server['server_status'] == 1 or $server['status_change'] == 0) ? ", status_change = " . time() : ", status_change = 10");
+        $str .= " WHERE server_id='{$server['server_id']}'";
+        echo "$str\n";
+        $result = db()->query($str);
         continue;
     }
     $servers_online++;
     $name = db()->escape_value($serv['name']);
-    $result = db()->query("
-  UPDATE " . DB_SERVERS . "
-   SET
-    server_name = '" . $name . "',
-    server_map = '" . $serv['map'] . "',
-    server_players = '" . $serv['players'] . "',
-    server_maxplayers = '" . $serv['max_players'] . "',
-    server_status = '1'
-  " . (($server['server_status'] == 0 or $server['status_change'] == 0) ? ", status_change = " . time() : ", status_change = 10") . "
-  WHERE server_id='" . $server['server_id'] . "'
- ");
+    $str = "UPDATE " . DB_SERVERS . " SET server_name = '{$name}',";
+    $str .= " server_map = '{$serv['map']}', server_players = '{$serv['players']}',";
+    $str .= " server_maxplayers = '{$serv['max_players']}', server_status = '1' ";
+    $str .= (($server['server_status'] == 0 or $server['status_change'] == 0) ? ", status_change = " . time() : ", status_change = 10");
+    $str .= " WHERE server_id='{$server['server_id']}'";
+    echo "$str\n";
+    $result = db()->query($str);
+    var_dump($result);
     if ($result) {
-        echo "<font color='green'>Даные сервера с порядковым " . $server['server_id'] . " внесены в базу данных</font>\n";
+        echo "<span style='color: green'>Даные сервера с порядковым " . $server['server_id'] . " внесены в базу данных</span>\n";
     } else {
-        echo "<font color='red'><b>Ошибка</b>, данные сервера с порядковым " . $server['server_id'] . " не были внесены в БД</font>\n";
+        echo "<span style='color: red'><b>Ошибка</b>, данные сервера с порядковым " . $server['server_id'] . " не были внесены в БД</span>\n";
     }
 }
 
