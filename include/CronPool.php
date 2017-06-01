dc806e03a56e2b0cbaf6d6316622721b1bb41bb7
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2017-06-02 01:01:05 +0300

    Fix

--- include/CronPool.php
+++ include/CronPool.php
@@ -11,15 +11,15 @@ class CronPool extends Pool
     private function destruct()
     {
         $servers = array();
-        $online = $this->connection->query("SELECT COUNT(*) FROM " . DB_SERVERS . "WHERE server_status = '1';");
+        $online = self::$connection->query("SELECT COUNT(*) FROM " . DB_SERVERS . "WHERE server_status = '1';");
         var_dump($online);
-        $sql = $this->connection->query("SELECT * FROM " . DB_SERVERS . ";");
-        while ($r = $this->connection->fetch_array($sql)) {
+        $sql = self::$connection->query("SELECT * FROM " . DB_SERVERS . ";");
+        while ($r = self::$connection->fetch_array($sql)) {
             $servers[] = $r;
         }
         //var_dump($servers);
         $map = topMap((array) $servers);
-        $result = $this->connection->query("
+        $result = self::$connection->query("
             UPDATE " . DB_SETTINGS . " SET
             last_update='" . time() . "',
             servers_total='" . count($servers) . "',
@@ -30,7 +30,7 @@ class CronPool extends Pool
 
     public function shutdown($hostname, $username, $password, $database, $charset, $port = 3306)
     {
-$this->connection = new mysqli(
+self::$connection = new mysqli(
                 $hostname,
                 $username,
                 $password,
@@ -38,7 +38,7 @@ $this->connection = new mysqli(
                 $port);
         }
 
-        $this->connection->set_charset($charset);
+        self::$connection->set_charset($charset);
         $this->destruct();
         parent::shutdown();
     }
