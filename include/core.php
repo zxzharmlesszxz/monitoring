23e9f4deeda0947aa6cf3fff94fad562d598cc2e
Harmless <harmless@syneforge.com>
2017-06-01 11:51:31 +0300

    Fix

--- include/core.php
+++ include/core.php
@@ -57,7 +57,7 @@ function db(){
 // Стили выделения
 $styles = Array();
 $get_styles = db()->query("SELECT * FROM `".DB_ROWSTYLES."` ORDER BY `id` DESC");
-while($style = db()->array_fetch($get_styles)) {
+while($style = db()->fetch_array($get_styles)) {
  $styles[$style['name']]['id'] = $style['id'];
  $styles[$style['name']]['title'] = $style['title'];
  $styles[$style['name']]['style'] = $style['style'];
