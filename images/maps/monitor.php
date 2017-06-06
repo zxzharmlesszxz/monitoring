8d7ccc645fffe4eb2ddbac89f51eb4dab3766dfa
Harmless <harmless@syneforge.com>
2017-06-06 18:43:03 +0300

    Fix

--- images/maps/monitor.php
+++ images/maps/monitor.php
@@ -6,7 +6,7 @@ $map = $_GET['map'];
 $game = $_GET['game'];
 $map_url = __DIR__ . "$game/$map.png";
 $background = imagecreatefrompng('monitor.png');
-$map_create = imagecreatefromjpeg($map_url);
+$map_create = imagecreatefrompng($map_url);
 $map_rotate = imagerotate($map_create, -20, 0);
 
 imagecopyresampled($image, $map_rotate, 100, 26, 0, 0, 160, 140, 191, 165);
