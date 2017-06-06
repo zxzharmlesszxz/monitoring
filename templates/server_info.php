aa6777930e0ee972af2d4c1c3d379f0948ef57bf
Harmless <harmless@syneforge.com>
2017-06-06 18:54:31 +0300

    Fix

--- templates/server_info.php
+++ templates/server_info.php
@@ -7,25 +7,25 @@
 
 /* Script security */
 if (!defined("MONENGINE")) {
- header("Location: index.php");
- exit();
+    header("Location: index.php");
+    exit();
 }
 
 /* Other code */
-require_once LOCALE.LOCALESET."serv.php";
+require_once LOCALE . LOCALESET . "serv.php";
 
 
 $site_link = SITE_URL;
 $server_id = $_GET['id'];
-$take_server = db()->query("SELECT * FROM ".DB_SERVERS." WHERE server_id = ".db()->escape_value($server_id)."");
+$take_server = db()->query("SELECT * FROM " . DB_SERVERS . " WHERE server_id = " . db()->escape_value($server_id) . "");
 $server_data = db()->fetch_array($take_server);
 
 if (db()->num_rows($take_server) == 0) {
- displayMessage('Выбранный сервер не существует, либо был удалён.', 'error');
+    displayMessage('Выбранный сервер не существует, либо был удалён.', 'error');
 } elseif ($server_data['server_off'] == 1) {
- include("banned.php");
+    include("banned.php");
 } else {
- $site = ($server_data['server_site'] !="") ? "
+    $site = ($server_data['server_site'] != "") ? "
   <div class='block_line' style='margin-bottom:0;'>
    <div class='block site'>
     <div class='t'>Сайт сервера:</div>
@@ -35,32 +35,32 @@ if (db()->num_rows($take_server) == 0) {
     </div>
    </div>" : "";
 
- $icq = (!empty($server_data['server_icq'])) ? "ICQ Администратора <b>ICQ#{$server_data['server_icq']} <img src='//status.icq.com/online.gif?icq={$server_data['server_icq']}&img=26'>" : "";
- $status = ($server_data['server_status'] == 1) ? "<span style='color: #6e8d4c'><b>Online</b></span>" : "<span style='color:#B53333'><b>Offline</b></span>";
- $work = (($server_data['server_status'] == 1) ? "Работает" : "Не работает")." с <b>".@date("d.m.Y H:i", $server_data['status_change'])."</b>";
- $regdate = @date("d.m.Y H:i", $server_data['server_regdata']);
- $last_update = $settings['last_update'];
- $time_diff = time() - $last_update;
- $time_lasted = ($time_diff >= 60) ? floor($time_diff / 60)." минут" : $time_diff." секунд";
- $percent_loaded = @floor(($server_data['server_players'] / $server_data['server_maxplayers']) * 100);
- $vote_hash = md5("m0n3ng1ne.s4lt:P{]we{$server_data['server_id']}@._)%;");
+    $icq = (!empty($server_data['server_icq'])) ? "ICQ Администратора <b>ICQ#{$server_data['server_icq']} <img src='//status.icq.com/online.gif?icq={$server_data['server_icq']}&img=26'>" : "";
+    $status = ($server_data['server_status'] == 1) ? "<span style='color: #6e8d4c'><b>Online</b></span>" : "<span style='color:#B53333'><b>Offline</b></span>";
+    $work = (($server_data['server_status'] == 1) ? "Работает" : "Не работает") . " с <b>" . @date("d.m.Y H:i", $server_data['status_change']) . "</b>";
+    $regdate = @date("d.m.Y H:i", $server_data['server_regdata']);
+    $last_update = $settings['last_update'];
+    $time_diff = time() - $last_update;
+    $time_lasted = ($time_diff >= 60) ? floor($time_diff / 60) . " минут" : $time_diff . " секунд";
+    $percent_loaded = @floor(($server_data['server_players'] / $server_data['server_maxplayers']) * 100);
+    $vote_hash = md5("m0n3ng1ne.s4lt:P{]we{$server_data['server_id']}@._)%;");
 
- switch($percent_loaded) {
-  case ($percent_loaded <= 40):
-   $load_color = 'green';
-   break;
-  case ($percent_loaded <= 80):
-   $load_color = 'yellow';
-   break;
-  case ($percent_loaded <= 100):
-   $load_color = 'red';
-   break;
-  default:
-   $load_color = 'green';
-   break;
- }
+    switch ($percent_loaded) {
+        case ($percent_loaded <= 40):
+            $load_color = 'green';
+            break;
+        case ($percent_loaded <= 80):
+            $load_color = 'yellow';
+            break;
+        case ($percent_loaded <= 100):
+            $load_color = 'red';
+            break;
+        default:
+            $load_color = 'green';
+            break;
+    }
 
- $about = !empty($server_data['about']) ? "
+    $about = !empty($server_data['about']) ? "
   <div class='box_rounded'>
    <div style='font-size: 10px;'>
     <div style='padding-bottom:3px;'>
@@ -70,74 +70,73 @@ if (db()->num_rows($take_server) == 0) {
   </div>
   <span style='color:#999999;'>{$server_data['about']}</span>" : "";
 
-  $com_error = '';
-  $errors = Array();
+    $com_error = '';
+    $errors = Array();
 
- $com_name = '';
- $com_text = '';
+    $com_name = '';
+    $com_text = '';
 
- if (count($_POST)>0) {
-  if (empty($_POST['com_name'])) {
-   $errors[] = 'Вы не ввели своё имя.';
-  } elseif (strlen($_POST['com_name']) < 2 or strlen($_POST['com_name']) > 12) {
-   $errors[] = 'Длина имени должна составлять от 2-х до 12-ти символов.';
-  } else {
-   $com_name = $_POST['com_name'];
-  }
+    if (count($_POST) > 0) {
+        if (empty($_POST['com_name'])) {
+            $errors[] = 'Вы не ввели своё имя.';
+        } elseif (strlen($_POST['com_name']) < 2 or strlen($_POST['com_name']) > 12) {
+            $errors[] = 'Длина имени должна составлять от 2-х до 12-ти символов.';
+        } else {
+            $com_name = $_POST['com_name'];
+        }
 
-  if (empty($_POST['com_text'])) {
-   $errors[] = 'Вы не ввели текст комментария.';
-  } elseif (strlen($_POST['com_text'] > 300)) {
-   $errors[] = 'Максимальная длина комментария 300 символов.';
-  } else {
-   $com_text = $_POST['com_text'];
-  }
+        if (empty($_POST['com_text'])) {
+            $errors[] = 'Вы не ввели текст комментария.';
+        } elseif (strlen($_POST['com_text'] > 300)) {
+            $errors[] = 'Максимальная длина комментария 300 символов.';
+        } else {
+            $com_text = $_POST['com_text'];
+        }
 
-  if (!isset($_SESSION['captcha_keystring']) or $_SESSION['captcha_keystring'] != $_POST['com_captcha']) {
-   $errors[] = 'Вы неверно ввели текст с картинки.';
-  }
+        if (!isset($_SESSION['captcha_keystring']) or $_SESSION['captcha_keystring'] != $_POST['com_captcha']) {
+            $errors[] = 'Вы неверно ввели текст с картинки.';
+        }
 
-  if (count($errors) != 0) {
-   $com_error = "<div style='font-size: 13px;color:white;padding: 5px;margin-bottom:7px;border: 1px solid#BC2E2E;background:#522828;text-align: left;'>".$errors[0]."</div>";
-  } else {
-   $server_id = db()->escape_value($server_id);
-   $com_name = db()->escape_value(htmlspecialchars($com_name));
-   $com_text = db()->escape_value(htmlspecialchars($com_text));
-   $result = db()->query("INSERT INTO ".DB_COMMENTS." (server_id, username, text, date) VALUES ('$server_id', '$com_name', '$com_text', '".time()."')");
-   $message = "<div style='font-size: 13px;color:white;padding: 5px;margin-bottom:7px;border: 1px solid#108014;background:#3A4337;text-align: left;'>Спасибо! Ваш комментарий будет добавлен после модерации.</div>";
-  }
- }
+        if (count($errors) != 0) {
+            $com_error = "<div style='font-size: 13px;color:white;padding: 5px;margin-bottom:7px;border: 1px solid#BC2E2E;background:#522828;text-align: left;'>" . $errors[0] . "</div>";
+        } else {
+            $server_id = db()->escape_value($server_id);
+            $com_name = db()->escape_value(htmlspecialchars($com_name));
+            $com_text = db()->escape_value(htmlspecialchars($com_text));
+            $result = db()->query("INSERT INTO " . DB_COMMENTS . " (server_id, username, text, date) VALUES ('$server_id', '$com_name', '$com_text', '" . time() . "')");
+            $message = "<div style='font-size: 13px;color:white;padding: 5px;margin-bottom:7px;border: 1px solid#108014;background:#3A4337;text-align: left;'>Спасибо! Ваш комментарий будет добавлен после модерации.</div>";
+        }
+    }
 
- unset($_SESSION['captcha_keystring']);
- $get_comments = db()->query("SELECT * FROM ".DB_COMMENTS." WHERE server_id='$server_id' and type!='0'");
+    unset($_SESSION['captcha_keystring']);
+    $get_comments = db()->query("SELECT * FROM " . DB_COMMENTS . " WHERE server_id='$server_id' and type!='0'");
 
- if (db()->num_rows($get_comments) != 0) {
-  $comms = '';
-  while ($comments = db()->fetch_array($get_comments)) {
-   $comms .= "<div class='comment'>
+    if (db()->num_rows($get_comments) != 0) {
+        $comms = '';
+        while ($comments = db()->fetch_array($get_comments)) {
+            $comms .= "<div class='comment'>
            <div class='comment_text'>Комментарий от:<span class='name'>{$comments['username']}</span><br />{$comments['text']}</div>
-           <p class='posttime'>Дата добавления: ".@date("d.m.Y H:i", $comments['date'])."</p>
-           <p class='".(($comments['type'] == 1) ? "positive" : "negative")."_post'>".(($comments['type'] == 1) ? "Позитивный" : "Негативный")." отзыв</p>
+           <p class='posttime'>Дата добавления: " . @date("d.m.Y H:i", $comments['date']) . "</p>
+           <p class='" . (($comments['type'] == 1) ? "positive" : "negative") . "_post'>" . (($comments['type'] == 1) ? "Позитивный" : "Негативный") . " отзыв</p>
            <div class='clearfix'></div>
           </div>";
-  }
- } else {
-  $comms = "<div class='comment'>
+        }
+    } else {
+        $comms = "<div class='comment'>
            <div class='comment_text'>Этот сервер пока что не имеет комментариев. Может вы хотели бы добавить свой?</div>
           </div>";
- }
+    }
 
- $mess = ((!empty($message)) ? "$message" : "").((!empty($com_error)) ? "$com_error" : '');
- $captch = session_name()."=".session_id();
+    $mess = ((!empty($message)) ? "$message" : "") . ((!empty($com_error)) ? "$com_error" : '');
+    $captch = session_name() . "=" . session_id();
 
- echo <<<EOT
+    echo <<<EOT
    <div class='horizontal_line'>Сервер: <b>{$server_data['server_name']}</b></div>
     <div class='cont'>
      <table cellspacing='0' cellpadding='0' width='100%' class='info_tbl'>
       <tr>
        <td valign='top' width='250' style='padding-bottom:20px;'>
         <div style='padding: 5px;margin: 4px;line-height: 12px;background: #1E1E1E;border: 1px solid #444;'>
-         <div style='padding-top:10px; margin: 0 auto;'></div>
           <img src='/images/maps/monitor.php?game={$server_data['server_game']}&map={$server_data['server_map']}' style='border:1px solid #7F7F7F;opacity:0.8;'>
         </div>
         <div style='text-align:center;padding-right:50px;'>
