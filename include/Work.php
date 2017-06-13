3bc10de6ced044820e3137b1bd0d848b126278dd
Harmless <harmless@syneforge.com>
2017-06-13 11:26:11 +0300

    Fix

--- include/Work.php
+++ include/Work.php
@@ -44,18 +44,20 @@ class Work extends Threaded
                 continue;
             }
 
-            if (($server['status'] == 'off' || empty($server['name'])) or !$site) {
-                $this->worker->getConnection()->real_query(
-                    "UPDATE " . DB_SERVERS . " SET
+            //if (($server['status'] == 'off' || empty($server['name'])) or !$site) {
+            if ($server['status'] == 'off' || empty($server['name'])) {
+                    $this->worker->getConnection()->real_query(
+                        "UPDATE " . DB_SERVERS . " SET
                     server_status = '0',
                     server_map = '-',
                     server_players = '-',
                     server_maxplayers = '-' "
-                    . (($server['server_status'] == 1) ? ", status_change = " . time() : "")
-                    . " WHERE server_id='{$server['server_id']}';"
-                );
-                //print "UPDATE " . DB_SERVERS . " SET server_status = '0', server_map = '-', server_players = '-', server_maxplayers = '-' " . (($server['server_status'] == 1) ? ", status_change = " . time() : "") . " WHERE server_id='{$server['server_id']}';" . PHP_EOL;
-                continue;
+                        . (($server['server_status'] == 1) ? ", status_change = " . time() : "")
+                        . " WHERE server_id='{$server['server_id']}';"
+                    );
+                    //print "UPDATE " . DB_SERVERS . " SET server_status = '0', server_map = '-', server_players = '-', server_maxplayers = '-' " . (($server['server_status'] == 1) ? ", status_change = " . time() : "") . " WHERE server_id='{$server['server_id']}';" . PHP_EOL;
+                    continue;
+                }
             }
 
             $name = $this->worker->getConnection()->real_escape_string(htmlspecialchars(trim($server['name'])));
