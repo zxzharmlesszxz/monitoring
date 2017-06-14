04d6309079d52c0f74acf9720bd3a95cf9e2a64c
Harmless <harmless@syneforge.com>
2017-06-14 17:26:10 +0300

    Fix

--- admin/send_email.php
+++ admin/send_email.php
@@ -2,8 +2,21 @@
 
 /* Script security */
 if (!defined("MONENGINE")) {
- header("Location: index.php");
- exit();
+    header("Location: index.php");
+    exit();
+}
+
+if (isset($_POST['submit'])) {
+    $servers = db()->query("SELECT * FROM `" . DB_SERVERS . "` WHERE `server_email` != '';");
+    $header_text = "Доброго времени суток.
+    Ваш сервер %s, id=%s , зарегистрирован %s, находится в нашем мониторинге https://www.monitoring.contra.net.ua.
+    Для улучшения Ваших позиций в рейтинге, предлагаем голосовать раз в сутки
+     для поднятия Вашего сервера на главной странице нашего мониторинга, чем выше сервер, тем больше человек посещает Ваш сервер.";
+    $footer_text = 'С уважением администрация мониторинга https://www.monitoring.contra.net.ua';
+    while ($server = db()->fetch_array($servers)) {
+        $message = sprintf($header_text, $server['server_name '], $server['server_id'], $server['server_regdata']);
+        echo "send_mail({$server['server_email']}, {$message}\n\n{$_POST['message']}\n\n{$footer_text})";
+    }
 }
 
 /* Other code */
