ebf6725b8433f43893c086d183c5075e8969774e
Harmless <harmless@syneforge.com>
2017-06-14 16:48:54 +0300

    Fix

--- admin/send_email.php
+++ admin/send_email.php
@@ -13,17 +13,16 @@ echo <<<EOT
    <div class='box'>
     <div class='title'>Рассылка почты<span class='hide'></span></div>
     <div class='content'>
-     $message
      <form action='' method='POST'>
       <div class='row'>
-       <label>Адрес сервера <span color='red'>*</span></label>
+       <label>Сообщение <span color='red'>*</span></label>
        <div class='right'>
-        <input type='text' name='server_address' placeholder='127.0.0.1:27015' value='$address'>
+        <textarea name='message'></textarea>
        </div>
       </div>
       <div class='row'>
        <div class='right'>
-        <button type='submit' name='submit_registration' class='blue'><span>Зарегистрировать</span></button>
+        <button type='submit' name='submit' class='blue'><span>Отправить</span></button>
         <button type='button' onClick='window.location.href='index.php'><span>Отмена</span></button>
        </div>
       </div>
