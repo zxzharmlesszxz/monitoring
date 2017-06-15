dd10c4bc8ab9caa76f1e93b222197b4ff124e7ff
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2017-06-15 23:18:14 +0300

    fix

--- templates/header.php
+++ templates/header.php
@@ -21,7 +21,7 @@ $title = (isset($page_title)) ? $page_title : $settings['site_name'];
     <meta NAME="Copyright" CONTENT="Мониторинг игровых серверов: WG">
     <meta NAME="Language" CONTENT="Russian">
     <meta NAME="Robots" CONTENT="All">
-    <!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="revisit-after" content="1 day">
     <meta name="distribution" content="Global">
     <meta name="rating" content="General">
