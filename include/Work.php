d8fe77055d17bfc6bb4ebaf0a5da995b7c7887b4
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2017-06-16 18:14:27 +0300

    Fix

--- include/Work.php
+++ include/Work.php
@@ -84,7 +84,7 @@ class Work extends Threaded
                 . (($server['server_status'] == 0) ? ", status_change = " . time() : "")
                 . " WHERE server_id='{$server['server_id']}';"
             );
-            print "UPDATE " . DB_SERVERS . " SET server_name = '{$name}', server_map = '{$server['map']}', server_players = '{$server['players']}', server_maxplayers = '{$server['max_players']}', server_status = '1' " . (($server['server_status'] == 0) ? ", status_change = " . time() : "") . " WHERE server_id='{$server['server_id']}';" . PHP_EOL;
+            print "UPDATE " . DB_SERVERS . " SET server_name = '{$name}', server_map = '{$server['mapName']}', server_players = '{$server['playerNumber']}', server_maxplayers = '{$server['maxPlayers']}', server_status = '1' " . (($server['server_status'] == 0) ? ", status_change = " . time() : "") . " WHERE server_id='{$server['server_id']}';" . PHP_EOL;
         } while ($value !== null);
     }
 
