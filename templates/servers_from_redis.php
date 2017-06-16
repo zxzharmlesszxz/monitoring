021a98f7f9af0cdc54b025f53e3dbc4841ebb929
Harmless <harmless@syneforge.com>
2017-06-16 12:36:51 +0300

    Test

--- templates/servers_from_redis.php
+++ templates/servers_from_redis.php
@@ -20,9 +20,9 @@ $servers = $redis->hGetAll('servers');
 if ($servers_total != 0) {
     $row = '';
     foreach ($servers as $id => $r) {
-        var_dump(unserialize($r)['info']);
-        /*
-        $players = $r['server_players'] . "/" . $r['server_maxplayers'];
+        $info = unserialize($r)['info'];
+
+        $players = $r['playerNumber'] . "/" . $r['maxPlayers'];
         $server_location = $r['server_location'];
 
         if (empty($server_location))
@@ -34,12 +34,12 @@ if ($servers_total != 0) {
             $row .= "<tr>";
         }
 
-        if ($r['server_players'] == $r['server_maxplayers']) {
-            $players = "<span style='color: #00FF00'>{$r['server_players']}/{$r['server_maxplayers']}</span>";
+        if ($r['playerNumber'] == $r['maxPlayers']) {
+            $players = "<span style='color: #00FF00'>{$r['playerNumber']}/{$r['maxPlayers']}</span>";
         }
 
         if ($r['server_status'] == 1) {
-            $server_full = floor(($r['server_players'] / $r['server_maxplayers']) * 100);
+            $server_full = floor(($r['playerNumber'] / $r['maxPlayers']) * 100);
         } else {
             $server_full = "0";
         }
@@ -63,32 +63,31 @@ if ($servers_total != 0) {
             $la = "la5";
 
         $row .= "<td align='left' style='padding-left:20px;'>";
-        $row .= "<img src='/images/flags/$server_location.png' class='location' title='{$r['server_location']}' alt='{$r['server_location']}'>";
-        $row .= "<a class='name' title='Перейти на страницу сервера {$r['server_name']}' href='" . $settings['site_url'] . "server/{$r['server_id']}' rel='follow'>" . htmlspecialchars($r['server_name']) . "</a> ";
+        $row .= "<img src='/images/flags/$server_location.png' class='location' title='{$server_location}' alt='{$server_location}'>";
+        $row .= "<a class='name' title='Перейти на страницу сервера {$r['serverName']}' href='" . $settings['site_url'] . "server/{$id}' rel='follow'>" . htmlspecialchars($r['serverName']) . "</a> ";
         $row .= (($r['server_steam'] == '1') ? '<img src=\'/images/img/icon_steam.png\'>' : '');
         $row .= "</td>";
         $row .= "<td><img src='/images/icons/{$r['server_game']}.gif' class='game' title='{$r['server_game']} сервер' alt='{$r['server_game']} сервер' />";
         $row .= "{$r['server_ip']}</td>";
         $row .= "<td class='mode'>{$r['server_mode']}</td>";
-        $row .= "<td class='map'>{$r['server_map']}<span class='icon' data-icon='" . MAPS . "{$r['server_game']}/{$r['server_map']}.png'></span></td>";
+        $row .= "<td class='map'>{$r['mapName']}<span class='icon' data-icon='" . MAPS . "{$r['server_game']}/{$r['mapName']}.png'></span></td>";
         $row .= "<td class='players'><span class='la'><img src='/images/la_icons/{$la}.gif'></span>{$players}</td>";
         $row .= "<td class='votes'>";
 
         if ($r['server_vip'] == 1) {
             $row .= '<img src="/images/img/vip.png" align="texttop" style="opacity:0.6;">';
         } else {
-            $row .= "<span class='votes_count' id='votes_count_{$r['server_id']}' >" . intval($r['votes']) . "</span>";
+            $row .= "<span class='votes_count' id='votes_count_{$id}' >" . intval($r['votes']) . "</span>";
             $row .= "<span class='vote_buttons' id='vote_buttons_{$r['server_id']}'>";
-            $row .= "<a href='javascript://' onClick=\"rating({$r['server_id']}, 'up', '" . md5("m0n3ng1ne.s4lt:P{]we{$r['server_id']}@._)%;") . "');\" class='voteup' id='{$r['server_id']}'></a>";
-            $row .= "<a href='javascript://' onClick=\"rating({$r['server_id']}, 'down', '" . md5("m0n3ng1ne.s4lt:P{]we{$r['server_id']}@._)%;") . "');\" class='votedown' id='{$r['server_id']}'></a>";
+            $row .= "<a href='javascript://' onClick=\"rating({$id}, 'up', '" . md5("m0n3ng1ne.s4lt:P{]we{$id}@._)%;") . "');\" class='voteup' id='{$id}'></a>";
+            $row .= "<a href='javascript://' onClick=\"rating({$id}, 'down', '" . md5("m0n3ng1ne.s4lt:P{]we{$id}@._)%;") . "');\" class='votedown' id='{$id}'></a>";
             $row .= "</span>";
         }
         $row .= "</td>";
         $row .= "</tr>";
-        */
     }
 }
-/*
+
 echo <<<EOT
 <table class='servers' cellspacing='0' border='0'>
  <thead>
