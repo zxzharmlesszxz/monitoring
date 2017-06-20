571b83aae4019d957eee5a049ae72c0774ff4a3b
Harmless <harmless@syneforge.com>
2017-06-20 16:45:59 +0300

    Fix

--- templates/servers_from_redis.php
+++ templates/servers_from_redis.php
@@ -17,7 +17,7 @@ if ($servers_total != 0) {
     uasort($servers, "sortByVotes");
 
     foreach ($servers as $id => $server) {
-        $server = json_decode($server);
+        $server = json_decode($server, true);
         $str = '';
         if ($server['info']['serverName'] == null)
             continue;
