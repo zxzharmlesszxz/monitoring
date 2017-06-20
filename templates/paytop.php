d6d20aa2f58e407638d415fb23d28afcd30ab4ee
Harmless <harmless@syneforge.com>
2017-06-20 12:14:36 +0300

    Fix

--- templates/paytop.php
+++ templates/paytop.php
@@ -14,8 +14,5 @@ echo <<<EOT
     <div class='horizontal_line'>Заказать «Премиум место»</div>
     <div class='cont'>
      <div class="msg greenbg">Приобрести данное место Вы можете <b>бесплатно</b>. Для заказа места обратитесь в Skype: <b>vengeanson</b></div>
-     <div>
-         <a href="/">Вернуться на главную страницу</a>
-     </div>
     </div>
 EOT;
