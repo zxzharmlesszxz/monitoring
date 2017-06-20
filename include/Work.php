5be5973f78cb4864bb097a79f5b9a32abeca8f88
Harmless <harmless@syneforge.com>
2017-06-20 11:33:57 +0300

    Fix

--- include/Work.php
+++ include/Work.php
@@ -41,7 +41,8 @@ class Work extends Threaded
             $players = $sq->getPlayers();
             $rules = $sq->getRules();
             $sq->disconnect();
-            $serverForRedis = serialize(array('info' => $info, 'players' => $players, 'rules' => $rules, 'dbInfo' => (array) $value));
+            //$serverForRedis = serialize(array('info' => $info, 'players' => $players, 'rules' => $rules, 'dbInfo' => (array) $value));
+            $serverForRedis = array('info' => $info, 'players' => $players, 'rules' => $rules, 'dbInfo' => (array) $value);
             $redisConnection->hSet('servers', $value['server_id'], $serverForRedis);
 
             $server = array_merge((array)$value, $info);
